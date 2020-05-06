4e1816e47696dd49fc7b8cc92c073218da5baabf
boot1110001 <ppuubblliicc@protonmail.com>
2020-05-06 19:50:06 +0200

    works

--- menu/menu.hs
+++ menu/menu.hs
@@ -1,18 +1,37 @@
 
+import System.IO
+
+-- prompt :: String -> IO String
+-- prompt text = do
+--     putStr text
+--     hFlush stdout
+--     getLine
+
+prompt2 :: String -> IO Integer
+prompt2 text = do
+    putStr text
+    hFlush stdout
+    readLn -- :: IO Integer
+
+printMenu :: IO ()
 printMenu = do
     putStrLn "--- Menú ---"
     putStrLn " 1 - Altas"
     putStrLn " 2 - Listados"
     putStrLn ""
     putStrLn " 0 - Fin"
+    putStrLn ""
 
+parseOpt :: Integer -> String
 parseOpt opt
-    | 0 putStrLn "Fin"
-    | 1 putStrLn "Altas"
-    | 2 putStrLn "Listado"
-    | otherwise putStrLn "Otro"
+    | opt == 0 = "Fin"
+    | opt == 1 = "Altas"
+    | opt == 2 = "Listado"
+    | otherwise = "Otro"
 
+main :: IO ()
 main = do
     printMenu
-
--- do {putStrLn "hola"; a <- getLine; print a}
+    opt <- prompt2 "Escribe una opción: "
+    putStrLn (parseOpt opt)
+    main
