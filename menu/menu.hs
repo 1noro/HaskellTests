6f830ad398a61993dd21bc6b50f76fb13d277488
boot1110001 <ppuubblliicc@protonmail.com>
2020-05-06 17:31:14 +0200

    sad

--- menu/menu.hs
+++ menu/menu.hs
@@ -1,2 +1,18 @@
-module Main where
-    main = putStrLn "Hello world!"
+
+printMenu = do
+    putStrLn "--- Menú ---"
+    putStrLn " 1 - Altas"
+    putStrLn " 2 - Listados"
+    putStrLn ""
+    putStrLn " 0 - Fin"
+
+parseOpt opt
+    | 0 putStrLn "Fin"
+    | 1 putStrLn "Altas"
+    | 2 putStrLn "Listado"
+    | otherwise putStrLn "Otro"
+
+main = do
+    printMenu
+
+-- do {putStrLn "hola"; a <- getLine; print a}
